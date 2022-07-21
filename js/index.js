510e4d66480d4c3810de90f0708251ca610cf5fc
斜曦 <zhangximin765@163.com>
2022-07-21 10:43:46 +0800

    随机数update

--- js/index.js
+++ js/index.js
@@ -2,17 +2,20 @@ document.addEventListener('DOMContentLoaded', () => {
     let cnarr = str.split('//')
     let enarr = eng.split('//')
     const userpost = getDom('.user_new_post')
-    var temparr = [];
+
+    var list = [];
     for (let i = 0; i < 5; i++) {
         let li = document.createElement('li')
         let acn = document.createElement('a')
         acn.href = 'javascript:;'
         let suiji = Math.round(Math.random() * (cnarr.length - 1) + 0);
         do {
+            //每次得到一个下标
             suiji = Math.round(Math.random() * (cnarr.length - 1) + 0);
-            temparr[i] = suiji;
-            acn.textContent = cnarr[suiji];
-        } while (!temparr.includes(suiji));
+            acn.textContent = cnarr[suiji];//将得到的内容的两边的空格去掉然后得到这个值。//李银河 李银河
+        } while (list.includes(suiji))
+        list.push(suiji)//我这里不写这个就死循环了
+        console.log(list);
         li.append(acn);
         acn.addEventListener('mouseenter', () => {
             acn.textContent = enarr[suiji]
